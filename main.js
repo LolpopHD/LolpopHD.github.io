6304c066a4e4ee5852cc8e003d2cdb6fdd5f7e92
LolpopHD <nicksavin2006@gmail.com>
2019-12-01 17:02:23 +0100

    V1.6.1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var consonants = ["b","c","d","f","g","h","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
 
 function encode(){
   var text = Caesar(false, 5, document.getElementById("input").value);
